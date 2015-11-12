e7e64377197d5de892837e88d93f857d937cf35a
Emmanuel BOTROS YOUSSEF <emmanuel.botros@gmail.com>
2015-11-12 01:53:02 +0100

    gplay integration poc

---
+++ gplay/js/constants.js
@@ -0,0 +1,40 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Todd Kerpelman
+ * Date: 10/15/12
+ * Time: 8:52 AM
+ * All game constants have been moved here, for ease of setting up your own
+ * application
+ *
+ * Copyright 2012 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+"use strict";
+
+var constants = constants || {};
+
+constants.ACH_PRIME = 'A1';
+constants.ACH_BORED ='A1';
+constants.ACH_HUMBLE = 'A1';
+constants.ACH_COCKY = 'A1';
+constants.ACH_LEET = 'A1';
+constants.ACH_REALLY_BORED = 'A1';
+
+constants.LEADERBOARD_EASY = 'L1';
+constants.LEADERBOARD_HARD = 'L1';
+
+constants.CLIENT_ID = '1073095352239-7vre9lsrt8s0bmg2ui9sfjpfcadmjre4.apps.googleusercontent.com';
+constants.APP_ID = '1073095352239';
+
+constants.LINK_PAGE_BASE = 'https://www.example.com/linkPage/index.php';
